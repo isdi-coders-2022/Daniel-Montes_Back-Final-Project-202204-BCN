823ce6da3bc885caf6baeea963a429812aa49b14
Daniel Montes <daniel.montes@gmail.com>
2022-06-07 22:39:13 +0200

    Change userController mw

--- src/server/middlewares/errors/errors.js
+++ src/server/middlewares/errors/errors.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const chalk = require("chalk");
-const debug = require("debug")(chalk.red("penguin:MW:errors:"));
+const debug = require("debug")(chalk.red("penguin:MW:errors"));
 
 const { customError } = require("../../utils/customError");
 
@@ -12,7 +12,9 @@ const notFoundError = (req, res, next) => {
 
 // eslint-disable-next-line no-unused-vars
 const generalError = (error, req, res, next) => {
-  debug(chalk.yellow(`ERROR: ${error.statusCode} -> ${error.message}`));
+  debug(
+    chalk.yellow(`ERROR: ${error.message} (statusCode: ${error.statusCode})`)
+  );
 
   const message = error.customMessage ?? error.message;
   const statusCode = error.statusCode ?? 500;
